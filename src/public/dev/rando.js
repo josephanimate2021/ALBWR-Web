98e1d964e30757f42e4d39d3e12776ddb857e473
Joseph Crosman <josephcrosmanplays@hotmail.com>
2025-04-21 19:36:37 -0400

    turning this app into a vite version with WebContainers in order to be able to run commands and have the required files all in the browser.

--- src/public/dev/rando.js
+++
@@ -1,3 +0,0 @@
-fetch(`/settings/dev/${repoName}`).then(res => res.json()).then(d => {
-    if (d.length == 0) document.getElementById('settingsLoading').innerHTML = 'No settings were loaded because the cloned repo does not exist'
-})
