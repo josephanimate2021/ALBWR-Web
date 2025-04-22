c7dc40680f896f21397e55715f81b6dc2d97d349
Joseph Crosman <josephcrosmanplays@hotmail.com>
2025-04-22 13:08:38 -0400

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    port: 80,
+    allowedHosts: ['z17randomizer.gonexus.xyz'],
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
-  base: '/ALBWR-Web/'
 });
